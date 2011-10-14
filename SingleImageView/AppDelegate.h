fad27ac1dfd0e16519b3c4793b0b97ba12ef5d3a
Lead Developer <dev@visiomedia.com>
2011-10-15 01:30:47 +0200

    underline, test only

--- SingleImageView/AppDelegate.h
+++ SingleImageView/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Frank Luchs on 15.10.11.
 //  Copyright (c) 2011 Visiomedia Ltd. All rights reserved.
 //
-
+//-------------------------------------------------------------------
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
